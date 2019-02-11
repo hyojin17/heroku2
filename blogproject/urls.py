e04eccc698891174f4eae4967f86a04628b3dbe6
hyojin17 <chj7960@likelion.org>
2019-02-11 13:04:44 +0900

    first commit

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('', blogapp.views.home, name="home"),
     path('blog/', include('blogapp.urls')),#blogapp안에있는 urls로부터 include해와라. 그리고 그 형식은 blog/로 하겠다.
     path('portfolio/', portfolio.views.portfolio, name="portfolio"),
+    path('portfolio/new_portfolio/', portfolio.views.new_portfolio, name="new_portfolio"),
+    path('portfolio/create_portfolio', portfolio.views.create_portfolio, name="create_portfolio"),
+
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .models import Portfolio
+from django.utils import timezone
 
 def portfolio(request):
     portfolios = Portfolio.objects
     return render(request, 'portfolio.html', {'portfolios':portfolios})
+
+def new_portfolio(request):
+    return render(request, 'new_portfolio.html')
+
+def create_portfolio(request):#입력받은 내용을 데이터베이스에 넣어주는 함수
+    portfolio = Portfolio()#Blog라는 클래스로부터 blog라는 객체를 하나 생성
+    portfolio.title = request.GET['title']#블로그 객체안에 title을 넣어준다.
+    portfolio.description = request.GET['description']
+    portfolio.image = request.GET['file']
+    # portfolio.pub_date = timezone.datetime.now()#이것을 쓰기 위해 위에 import해준다.
+    portfolio.save()#지금까지 객체에 넣은 내용을 데이터베이스에 저장해라
+    return redirect('/portfolio')#여기 다시 고치기-이미지 업로더 해야 가능.
